05114296c982c0291e5775321d65dc6cb7eebe86
AnonymousMorris <morrislin13@gmail.com>
2024-01-07 19:38:12 +0000

    main.py calls api to get list of problems given the url of the contest scraper.py will return a list of 2 strings, the example input and the example output text

---
+++ scraper.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_input_output(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    Example = soup.find("div", {"class": "sample-test"})
+    raw_input_text = Example.find_all("div", {"class": "test-example-line"})
+    input_text = ""
+    for line in raw_input_text:
+        input_text += (line.text + "\n")
+    print(input_text)
+    output = Example.find("div", {"class": "output"})
+    output_text = output.text
+    print(output_text)
+    ans = [input_text, output_text]
+    return ans
